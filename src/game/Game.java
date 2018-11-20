7225beca173336f3b12de208ff1b09fc56e6bdd3
Sava Filipovic <savafilipovic@gmail.com>
2018-11-20 11:42:44 +0000

    fillen out all the fields

--- src/game/Game.java
+++ src/game/Game.java
@@ -21,10 +21,38 @@ public class Game {
                 occupiedSpots++;
             }
         }
-
-		for(int i = 0; i < this.h;i++){
+        for(int i = 0; i < this.h;i++){
 			for (int j = 0; j < this.w; j++) {
-				this.grid[i][j] = new Block(matrix[i][j]);
+			    if (this.matrix[i][j] == -1)
+				    this.grid[i][j] = new Block(matrix[i][j]);
+			    else {
+			        int bombCount = 0;
+			        if (i != 0) {
+			            bombCount += (matrix[i-1][j] == -1? 1 : 0);
+			            if (j != 0) {
+                            bombCount += (matrix[i-1][j-1] == -1? 1 : 0);
+                        }
+                        if (j + 1 != this.w) {
+                            bombCount += (matrix[i-1][j+1] == -1? 1 : 0);
+                        }
+                    }
+                    if (i + 1 != h) {
+                        bombCount += (matrix[i+1][j] == -1? 1 : 0);
+                        if (j != 0) {
+                            bombCount += (matrix[i+1][j-1] == -1? 1 : 0);
+                        }
+                        if (j + 1 != this.w) {
+                            bombCount += (matrix[i+1][j+1] == -1? 1 : 0);
+                        }
+                    }
+                    if (j != 0) {
+                        bombCount += (matrix[i][j-1] == -1? 1 : 0);
+                    }
+                    if (j + 1 != this.h) {
+                        bombCount += (matrix[i][j+1] == -1? 1 : 0);
+                    }
+                    this.grid[i][j] = new Block(bombCount);
+                }
 			}
 		}
 	}
